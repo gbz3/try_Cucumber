d1dc50ddae8cecc0ddbbb5080a2f05f2e6e1e07f
gbz3 <1808483+gbz3@users.noreply.github.com>
2025-04-05 10:34:06 +0900

    Mokito

--- hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -3,6 +3,7 @@ package hellocucumber;
 import io.cucumber.java.en.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class IsItFriday {
     static String isItFriday(String today) {
@@ -29,5 +30,40 @@ public class StepDefinitions {
     public void i_should_be_told(String expected) {
         assertEquals(expected, actual);
     }
-    
+
+    /// ///////////////////////////////////////////
+    private Account account;
+    private int withdrawAmount;
+    private final BankService bankService = mock(BankService.class);
+    private boolean withdrawSuccess;
+
+    @Given("口座残高が{int}円である")
+    public void givenInitialBalance(int initialBalance) {
+        account = new Account(initialBalance, bankService);
+    }
+
+    @Given("引き出し金額が{int}円である")
+    public void givenWithdrawAmount(int amount) {
+        withdrawAmount = amount;
+    }
+
+    @When("お金を引き出す")
+    public void when() {
+        try {
+            account.withdraw(withdrawAmount);
+            withdrawSuccess = true;
+        } catch (Exception e) {
+            withdrawSuccess = false;
+        }
+    }
+
+    @Then("口座残高は{int}円になる")
+    public void thenBalance(int expectedBalance) {
+        assertEquals(expectedBalance, account.getBalance());
+    }
+
+    @Then("銀行サービスは引き出し処理を{int}回呼び出す")
+    public void thenVerifyCall(int times) {
+        verify(bankService, times(times)).withdraw(account, withdrawAmount);
+    }
 }
